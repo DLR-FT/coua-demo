aecf6fe7717a7c8b5086866691d8a1a7106448eb
Tim Schubert <dadada@dadada.li>
2024-12-10 16:04:56 +0100

    3: add documentation and use coua results

---
+++ doc/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'my-hello-world'
+copyright = '2024, Tim'
+author = 'Tim'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["coua"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Coua --
+coua_load = [("../../coua.nt", "application/n-triples")]
---
+++ doc/source/index.rst
@@ -0,0 +1,24 @@
+.. my-hello-world documentation master file, created by
+   sphinx-quickstart on Mon Dec  9 17:59:53 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+my-hello-world documentation
+============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+.. coua:requirements_section::
+
+
+Source Code Requirements Tracing
+--------------------------------
+.. coua:source_code_tracability_matrix::
